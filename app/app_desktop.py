e3e8e24c5e87daae7fe6e61340c79c9a00a5b8a1
sush1 <pavelsirotkin@MacBook-Air.local>
2023-10-01 14:51:44 +0300

    feat: добавил основной функционал десктоп приложения

--- app/app_desktop.py
+++ app/app_desktop.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from .app import *
+from tkinter import messagebox
+
 
 class DesktopApp(App):
     def __init__(self, db_address) -> None:
@@ -11,30 +13,38 @@ class DesktopApp(App):
         self.category_id = None
         self.price = None
         self.amount = None
+        # лейблы
+        self.product_name_label = None
+        self.category_id_label = None
+        self.price_label = None
+        self.price_label = None
         # кнопки
         self.save_button = None
         self.delete_button = None
         self.update_button = None
-        self.show_all_button = None
+        self.amount_label = None
         
         self._create_entries()
         self._create_buttons()
+        self._init_elements(
+            self.product_name_label, self.product_name, self.category_id_label, self.category_id, 
+            self.price_label, self.price, self.amount_label, self.amount, self.save_button, 
+            self.delete_button, self.update_button, self.show_all_button
+        )
 
         # Запускаем главный цикл обработки событий
         self.tk.mainloop()
 
     def _create_entries(self):
         # Создаем поля ввода
+        self.product_name_label = tk.Label(self.tk, text='Название продукта')
         self.product_name = tk.Entry(self.tk)
+        self.category_id_label = tk.Label(self.tk, text='ID категории')
         self.category_id = tk.Entry(self.tk)
+        self.price_label = tk.Label(self.tk, text='Стоимость')
         self.price = tk.Entry(self.tk)
+        self.amount_label = tk.Label(self.tk, text='Количество')
         self.amount = tk.Entry(self.tk)
-
-        # Отображение полей ввода на форме
-        self.product_name.pack(),
-        self.category_id.pack(),
-        self.price.pack(),
-        self.amount.pack()
         
     def _create_buttons(self):
         # Создаем кнопку
@@ -42,11 +52,10 @@ class DesktopApp(App):
         self.delete_button = tk.Button(self.tk, text="Удалить", command=self.delete)
         self.update_button = tk.Button(self.tk, text="Обновить", command=self.update)
         self.show_all_button = tk.Button(self.tk, text="Показать все", command=self.show_all)
-        
-        self.save_button.pack(),
-        self.delete_button.pack(),
-        self.update_button.pack(),
-        self.show_all_button.pack()
+
+    def _init_elements(self, *args):
+        for element in args:
+            element.pack()
 
     def save(self):
         return self.app.save(self.product_name.get(), self.category_id.get(), self.price.get(), self.amount.get())
@@ -59,4 +68,5 @@ class DesktopApp(App):
 
     def show_all(self):
         print(self.app.show_all())
+        messagebox.showinfo('Данные в БД', self.app.show_all())
 
--- main.py
+++ main.py
@@ -3,3 +3,5 @@ from app import *
 
 if __name__ == '__main__':
     test_app = DesktopApp('test-db')
+
+    print('smth')
